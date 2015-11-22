5414bebd14ddc26ac58c274aac92142ab5928dc6
Raymundo Cortez <rc37495@utexas.edu>
2015-11-22 00:13:07 -0600

    no message

--- 03 Shiny/server.R
+++ 03 Shiny/server.R
@@ -127,42 +127,10 @@ shinyServer(function(input, output) {
   
   # Begin code for Third Tab:
   
-  df3 <- eventReactive(input$clicks3, {data.frame(fromJSON(getURL(URLencode(gsub("\n", " ", 'skipper.cs.utexas.edu:5001/rest/native/?query=
-                                                                                 """select region || \\\' \\\' || \\\'Sales\\\' as measure_names, sum(sales) as measure_values from SUPERSTORE_SALES_ORDERS
-                                                                                 where country_region = \\\'United States of America\\\'
-                                                                                 group by region
-                                                                                 union all
-                                                                                 select market || \\\' \\\' || \\\'Coffee_Sales\\\' as measure_names, sum(coffee_sales) as measure_values from COFFEE_CHAIN
-                                                                                 group by market
-                                                                                 order by 1;"""
-                                                                                 ')), httpheader=c(DB='jdbc:oracle:thin:@sayonara.microlab.cs.utexas.edu:1521:orcl', USER='C##cs329e_UTEid', PASS='orcl_UTEid', 
-                                                                                                   MODE='native_mode', MODEL='model', returnDimensions = 'False', returnFor = 'JSON'), verbose = TRUE)))
-  })
+  df3 <- eventReactive(input$clicks3, {})
   
   output$distPlot3 <- renderPlot(height=1000, width=2000, {
-    plot3 <- ggplot() + 
-      coord_cartesian() + 
-      scale_x_discrete() +
-      scale_y_continuous() +
-      #facet_wrap(~CLARITY, ncol=1) +
-      labs(title='Blending 2 Data Sources') +
-      labs(x=paste("Region Sales"), y=paste("Sum of Sales")) +
-      layer(data=df3(), 
-            mapping=aes(x=MEASURE_NAMES, y=MEASURE_VALUES), 
-            stat="identity", 
-            stat_params=list(), 
-            geom="bar",
-            geom_params=list(colour="blue"), 
-            position=position_identity()
-      ) + coord_flip() +
-      layer(data=df3(), 
-            mapping=aes(x=MEASURE_NAMES, y=MEASURE_VALUES, label=round(MEASURE_VALUES)), 
-            stat="identity", 
-            stat_params=list(), 
-            geom="text",
-            geom_params=list(colour="black", hjust=-0.5), 
-            position=position_identity()
-      )
+    plot3 <- ggplot()
     plot3
   })
 })
